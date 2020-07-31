1ef23b4812668f6ff8c03da323e700b0f451cf11
jjangse <jjangse@DESKTOP-JJED6DS>
2020-07-31 18:46:49 +0900

    구구단

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
 public class Gugudan {
-	public static void gugudan(int x) {	
-		for (int i = 1; i < 10; i++) {
-			System.out.println(x + " * "+ i + " = " + x*i);
-		}
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void gugudan() {	
 		Scanner sc = new Scanner(System.in);
-		int num;
+		System.out.println("몇 단 까지?");
+		int maxNum = sc.nextInt();
+		System.out.println();
+		int maxNum2 = sc.nextInt();
 		
-		System.out.println("출력할 단은?");
-		num = sc.nextInt();
-		
-		gugudan(num);
-	}
-
+		if (maxNum < maxNum2) {
+			System.out.println("틀린 입력입니다.");
+			System.out.println();
+			gugudan();
+		}else {
+			int num[] = new int[maxNum];
+			for(int j = 0; j < num.length; j++) {
+				for (int i = 0; i < maxNum2; i++) {
+					num[i] = (i+1)*(j+1);
+					System.out.println((j+1)+" * "+(i+1)+" = " + num[i]);
+				}
+				System.out.println();
+			}
+		}
+	}	
 }
