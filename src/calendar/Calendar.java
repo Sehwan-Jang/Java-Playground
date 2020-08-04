8cbb43865a9d40a7027ad50e2cb07698c71dfba4
jjangse <jjangse@DESKTOP-JJED6DS>
2020-08-04 13:02:58 +0900

    calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,5 +3,16 @@ package calendar;
 public class Calendar {
 	public static void main(String[] args) {
 		System.out.println("Hello, Calendar");
+		System.out.print("일"+"\t"+"월"+"\t"+"화"+"\t"+"수"+"\t"
+				+"목"+"\t"+"금"+"\t"+"토");
+		System.out.println();
+		System.out.println("--------------------------------------------------");
+		for (int i = 1; i < 32; i++) {
+			int j = i;
+			System.out.print(j+ "\t");
+			if (j%7 ==0) {
+				System.out.println();
+			}
+		}
 	}
 }
