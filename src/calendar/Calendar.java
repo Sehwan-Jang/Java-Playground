167c8602b6c2607c4502faf10a6370fd31b3a074
jjangse <jjangse@DESKTOP-JJED6DS>
2020-08-08 16:44:54 +0900

    added startday method

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,38 +3,58 @@ package calendar;
 public class Calendar {
 	private static final int[] DAY = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_DAY = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final String[] FIRST_DAY = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
 
-	public void printCalendar(int year, int month) {
+	public boolean isLeapYear(int year) {
+		if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+			return true;
+		else
+			return false;
+	}
+
+	public int getMaxDayOfMonth(int year, int month) {
+		if (isLeapYear(year))
+			return LEAP_DAY[month - 1];
+		else
+			return DAY[month - 1];
+	}
+
+	public int getFirstDay(String fday) {
+		int firstDay = 0;
+		String day = fday.toUpperCase();
+		for (int i = 0; i < FIRST_DAY.length; i++) {
+			if (day.equals(FIRST_DAY[i])) {
+				firstDay = i;
+				break;
+			}
+		}
+		return firstDay;
+	}
+
+	public void printCalendar(int year, int month, String day) {
 		System.out.printf("                <<%4d년%7d월>>%n", year, month);
 		System.out.print("SU" + "\t" + "MO" + "\t" + "TU" + "\t" + "WE" + "\t" + "TH" + "\t" + "FR" + "\t" + "SA");
 		System.out.println();
 		System.out.println("--------------------------------------------------");
 
-		if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
-			for (int i = 1; i <= LEAP_DAY[month - 1]; i++) {
-				if (i < 10) {
-					System.out.print(" " + i + "\t");
-				} else
-					System.out.print(i + "\t");
-				if (i % 7 == 0) {
-					System.out.println();
-				}
-			}
-			System.out.println();
-			System.out.println();
-		} else {
-			for (int i = 1; i <= DAY[month - 1]; i++) {
-				if (i < 10) {
-					System.out.print(" " + i + "\t");
-				} else
-					System.out.print(i + "\t");
-				if (i % 7 == 0) {
-					System.out.println();
-				}
-			}
-			System.out.println();
-			System.out.println();
+		int maxDay = getMaxDayOfMonth(year, month);
+		int firstDay = getFirstDay(day);
+
+		for (int i = 0; i < firstDay; i++) {
+			System.out.print("  " + "\t");
 		}
+
+		for (int i = 1; i <= maxDay; i++) {
+			if (i < 10) {
+				System.out.print(" " + i + "\t");
+			} else
+				System.out.print(i + "\t");
+			if ((firstDay + i) % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+		System.out.println();
 
 	}
 }
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -3,52 +3,45 @@ package calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	
-	private final static String PROMPT = "cal> ";
-	
-	public static void runPrompt() {
+
+	public void runPrompt() {
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		// 반복할 횟수 입력 = for문 사용할 때 필요한 코드 
-		/*
-		 * System.out.println("반복할 횟수를 입력하세요."); int num = sc.nextInt();
-		 */
+		int year;
+		int month;
+		String day;
 
-		// 강의에서 쓴 array version + loop
-		int month = 0;
-		
 		while (true) {
-			System.out.println("월을 입력해주세요. (종료하려면 -1을 입력하세요)");
-			System.out.print(PROMPT);
-			month = sc.nextInt();
-			if(month==-1) 
+			System.out.println("년도를 입력해주세요. (종료하려면 -1을 입력하세요)");
+			System.out.print("year=> ");
+			year = sc.nextInt();
+			if (year == -1)
 				break;
-			if (month<1 || month> Calendar.day.length) {
+			System.out.println("월을 입력해주세요.");
+			System.out.print("month=> ");
+			month = sc.nextInt();
+			System.out.println("첫번째 요일을 입력해주세요. (SU, MO, TU, WE, TH, FR, SA)");
+			System.out.print("day=> ");
+			day = sc.next();
+			System.out.println();
+
+			if (month < 1 || month > 12 || year < 0) {
 				System.out.println("입력 오류입니다.");
 				System.out.println();
 				continue;
 			}
-			System.out.printf("%d월은 %d일 까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
-			System.out.println();
-			cal.printSampleCalendar(month);
+
+			cal.printCalendar(year, month, day);
 		}
 		System.out.println();
 		System.out.println("Bye~");
 
-		// 조건문 활용한 코드
-		/*
-		 * int days = 0; if (month < 1 || month > 12) { System.out.println("오류입니다"); }
-		 * else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8
-		 * || month == 10 || month == 12) { days = 31;
-		 * System.out.printf("%d월은 %d까지 있습니다.", month, days); } else if (month == 2) {
-		 * days = 28; System.out.printf("%d월은 %d까지 있습니다.", month, days); } else { days =
-		 * 30; System.out.printf("%d월은 %d까지 있습니다.", month, days); }
-		 */
-		sc.close();	
+		sc.close();
 	}
-		
+
 	public static void main(String[] args) {
-		runPrompt();
+		Prompt prompt = new Prompt();
+		prompt.runPrompt();
 	}
 }
